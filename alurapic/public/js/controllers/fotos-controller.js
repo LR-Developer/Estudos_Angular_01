ed5af7a1fb1650e548a00ba989940d9f3d8c0c38
LR-Developer <lr.developer@outlook.com>
2018-05-06 11:58:20 -0300

    Aula 03: * Chamando api (GET) para popular lista de imagens.

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,7 +1,12 @@
-angular.module('alurapic').controller('FotosController', function($scope){
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
    
-    $scope.foto = {
-        titulo: 'Leão',
-        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
+    $scope.fotos = [];
+
+    $http.get('/v1/fotos')
+        .success(function(retorno) {
+            $scope.fotos = retorno;
+        })
+        .error(function(error) {
+            console.log(error);
+        });
 });
