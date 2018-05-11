2601e4a11d4471a7aaf2755ae1c4af5b660d1c59
LR-Developer <luis_vtds@hotmail.com>
2018-05-11 20:43:44 -0300

    Aula 10: * Refatoração de uso do $http para o uso de resource.

--- alurapic/public/js/controllers/foto-controller.js
+++ alurapic/public/js/controllers/foto-controller.js
@@ -1,48 +1,39 @@
 angular.module('alurapic')
-    .controller('FotoController', function($scope, $http, $location, $routeParams) {
+    .controller('FotoController', function($scope, recursoFoto, $routeParams) {
+
         $scope.foto = {};
         $scope.mensagem = '';
 
         if($routeParams.fotoId) {
 
-        };
-
-        if($routeParams.fotoId) {
-
-            $http.get('/v1/fotos/' + $routeParams.fotoId)
-            .success(function(foto) {
+            recursoFoto.get({ fotoId: $routeParams.fotoId }, function(foto){
                 $scope.foto = foto;
-            })
-            .error(function(erro) {
+            }
+            ,function(erro) {
                 console.log(erro);
                 $scope.mensagem = 'Não foi possível obter a foto'
             });
         }
 
         $scope.submeter = function() {
-            
-            if($scope.formulario.$valid) {
-                
+
+            if ($scope.formulario.$valid) {
+
                 if($routeParams.fotoId) {
 
-                    $http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
-                    .success(function() {
+                    recursoFoto.update({fotoId: $scope.foto._id}, 
+                        $scope.foto, function() {
                         $scope.mensagem = 'Foto alterada com sucesso';
-                    })
-                    .error(function(erro) {
+                    }, function() {
                         console.log(erro);
                         $scope.mensagem = 'Não foi possível alterar';
                     });
-                }
-                else {
-                    $http.post('/v1/fotos', $scope.foto)
-                    .success(function() {
-                        $scope.mensagem = 'Foto adicionada com sucesso';
+
+                } else {
+                    recursoFoto.save($scope.foto, function() {
                         $scope.foto = {};
-                        $scope.formulario.$setPristine(); 
-                        $scope.formulario.$setUntouched();
-                    })
-                    .error(function(erro) {
+                        $scope.mensagem = 'Foto cadastrada com sucesso';
+                    }, function(erro) {
                         console.log(erro);
                         $scope.mensagem = 'Não foi possível cadastrar a foto';
                     });
